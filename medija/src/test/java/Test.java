c9d059ed671bb1151f79fd4040e2a60675483295
p9yp9y <p9yp9y@gmail.com>
2017-06-06 12:29:45 +0200

    Update Test.java

--- medija/src/test/java/Test.java
+++ medija/src/test/java/Test.java
@@ -7,7 +7,7 @@ public class Test {
 	@org.junit.Test
 	public void testPrintChannels() throws IOException, InterruptedException {
 		HtsClient htsClient = new HtsClient();
-		htsClient.open("pgy.no-ip.hu", 9982, "andris", "pender123", () -> printChannels(htsClient));
+		htsClient.open("pgy.no-ip.hu", 9982, "andris", "", () -> printChannels(htsClient));
 		waitForFinish(htsClient);
 	}
 
